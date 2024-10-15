b541e341275befdc612db1ecdae407be19f91957
ALI <alicloner@hotmail.com>
2024-10-15 13:37:06 +0330

    Update mineclaimer.py

--- bot/mineclaimer.py
+++ bot/mineclaimer.py
@@ -1,18 +1,17 @@
 import time
 import random
-import asyncio
 from bot.utils import night_sleep, Colors
 
-async def mine_claimer(NotPxClient, session_name):
-    await asyncio.sleep(5)  # Start with a delay...
+def mine_claimer(NotPxClient, session_name):
+    time.sleep(5)  # Start with a delay...
     print("[+] {}Auto claiming started{}.".format(Colors.CYAN, Colors.END))
     while True:
-        await night_sleep()  # Check and sleep if it's between 12-1 AM Iran time
-        acc_data = await NotPxClient.accountStatus()
+        night_sleep()  # Check and sleep if it's between 12-1 AM Iran time
+        acc_data = NotPxClient.accountStatus()
         
         if acc_data is None:
             print("[!] {}{}{}: {}Failed to retrieve account status. Retrying...{}".format(Colors.CYAN, session_name, Colors.END, Colors.RED, Colors.END))
-            await asyncio.sleep(5)
+            time.sleep(5)
             continue
         
         if 'fromStart' in acc_data and 'speedPerSecond' in acc_data:
@@ -22,7 +21,7 @@ async def mine_claimer(NotPxClient, session_name):
             random_recharge_speed = random.randint(30,90)
 
             if fromStart * speedPerSecond > 0.3:
-                claimed_count = round(await NotPxClient.claim_mining(), 2)
+                claimed_count = round(NotPxClient.claim_mining(), 2)
                 print("[+] {}{}{}: {} NotPx Token {}Mined{}.".format(
                     Colors.CYAN, session_name, Colors.END,
                     claimed_count, Colors.GREEN, Colors.END
@@ -31,4 +30,4 @@ async def mine_claimer(NotPxClient, session_name):
             print("[!] {}{}{}: {}Unexpected account data format. Retrying...{}".format(Colors.CYAN, session_name, Colors.END, Colors.RED, Colors.END))
         
         print("[!] {}{}{}: Sleeping for {} minutes...".format(Colors.CYAN, session_name, Colors.END,round((maxMiningTime+random_recharge_speed)/60),2))
-        await asyncio.sleep(maxMiningTime+random_recharge_speed)
+        time.sleep(maxMiningTime+random_recharge_speed)
