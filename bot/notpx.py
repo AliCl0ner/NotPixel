0868df6c0a22bc5974bd89cde20ede078bdd6d94
ALI <alicloner@hotmail.com>
2024-10-15 13:37:21 +0330

    Update notpx.py

--- bot/notpx.py
+++ bot/notpx.py
@@ -38,16 +38,25 @@ class NotPx:
                 raise SystemExit(f"{Colors.RED}[ERROR]{Colors.END} Unexpected error. Exiting...")
 
         self.session_name = session_name
-
-    async def init_async(self):
-        await self.__update_headers()
-
-    async def __update_headers(self):
-        async with TelegramClient(self.session_name, config.API_ID, config.API_HASH) as client:
-            WebAppQuery = await self.GetWebAppData(client)
-
+        self.__update_headers()
+        
+    def __update_headers(self):
+        client = TelegramClient(self.session_name,config.API_ID,config.API_HASH).start()
+        WebAppQuery = client.loop.run_until_complete(self.GetWebAppData(client))
+        client.disconnect()
         self.session.headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.9',
             'Authorization': f'initData {WebAppQuery}',
+            'Priority': 'u=1, i',
+            'Referer': 'https://notpx.app/',
+            'Sec-Ch-Ua': 'Chromium;v=119, Not?A_Brand;v=24',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': 'Linux',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.105 Safari/537.36',
         }
 
     async def GetWebAppData(self, client):
@@ -58,7 +67,7 @@ class NotPx:
         webappdata_global = webappdata_global.replace(user_data, unquote(user_data))
         return webappdata_global
 
-    async def request(self, method, end_point, key_check, data=None):
+    def request(self, method, end_point, key_check, data=None):
         try:
             if method == "get":
                 response = self.session.get(f"https://notpx.app/api/v1{end_point}", timeout=15)
@@ -68,7 +77,7 @@ class NotPx:
             # Handle NotPixel heavy load error
             if "failed to parse" in response.text:
                 print("[x] {}NotPixel internal error. Wait 5 minutes...{}".format(Colors.RED, Colors.END))
-                await asyncio.sleep(5 * 60)  # Use asyncio.sleep instead of time.sleep
+                time.sleep(5 * 60)  # Use asyncio.sleep instead of time.sleep
             elif response.status_code == 200:
                 # print(response.json())
                 if key_check in response.text:
@@ -76,19 +85,19 @@ class NotPx:
                 else:
                     raise Exception(report_bug_text.format(response.text))
             elif response.status_code >= 500:
-                await asyncio.sleep(5)  # Use asyncio.sleep for non-blocking sleep
+                time.sleep(5)  # Use asyncio.sleep for non-blocking sleep
             else:
                 # Renew authentication within the existing event loop
                 try:
-                    async with TelegramClient(self.session_name, config.API_ID, config.API_HASH) as client:
-                        WebAppQuery = await self.GetWebAppData(client)
+                    with TelegramClient(self.session_name, config.API_ID, config.API_HASH) as client:
+                        WebAppQuery = self.GetWebAppData(client)
 
                     # Update headers with the renewed authentication
                     self.session.headers.update({
                         "Authorization": "initData " + WebAppQuery
                     })
                     print("[+] Authentication renewed!")
-                    await asyncio.sleep(2)  # Use 'asyncio.sleep' for non-blocking sleep
+                    time.sleep(2) 
                 except Exception as e:
                     print(f"[-] Error during authentication renewal: {e}")
 
@@ -96,41 +105,36 @@ class NotPx:
                 urllib3.exceptions.NewConnectionError, 
                 requests.exceptions.Timeout) as e:
             print(f"[!] {Colors.RED}{type(e).__name__}{Colors.END} {end_point}. Sleeping for 5s...")
-            await asyncio.sleep(5)
+            time.sleep(5)
 
-    async def claim_mining(self):
-        response = await self.request("get", "/mining/claim", "claimed")  # Await the request
-        return response['claimed']  # Access the result after awaiting
+    def claim_mining(self):
+        return self.request("get", "/mining/claim", "claimed")['claimed'] 
 
-    async def accountStatus(self):
-        response = await self.request("get", "/mining/status", "speedPerSecond")
-        return response
+    def accountStatus(self):
+        return self.request("get", "/mining/status", "speedPerSecond")
+        
 
-    async def pixelStatus(self, pixelid):
-        response = await self.request("get", f"/image/get/{pixelid}", "isAvailable")
-        return response
+    def pixelStatus(self, pixelid):
+        return self.request("get", f"/image/get/{pixelid}", "isAvailable")
 
-    async def autoPaintPixel(self):
+    def autoPaintPixel(self):
         colors = ["#FFFFFF", "#000000", "#00CC78", "#BE0039"]
         random_pixel = (random.randint(100, 990) * 1000) + random.randint(100, 990)
         data = {"pixelId": random_pixel, "newColor": random.choice(colors)}
 
-        response = await self.request("post", "/repaint/start", "balance", data)
-        return response['balance']
+        return self.request("post", "/repaint/start", "balance", data)['balance']
 
-    async def paintPixel(self, pixelformated, hex_color):
+    def paintPixel(self, pixelformated, hex_color):
         data = {"pixelId": pixelformated, "newColor": hex_color}
-        response = await self.request("post", "/repaint/start", "balance", data)
+        response = self.request("post", "/repaint/start", "balance", data)
+        print(response)
         return response['balance']
 
-    async def upgrade_paintreward(self):
-        response = await self.request("get", "/mining/boost/check/paintReward", "paintReward")
-        return response['paintReward']
+    def upgrade_paintreward(self):
+        return self.request("get", "/mining/boost/check/paintReward", "paintReward")['paintReward']
 
-    async def upgrade_energyLimit(self):
-        response = await self.request("get", "/mining/boost/check/energyLimit", "energyLimit")
-        return response['energyLimit']
+    def upgrade_energyLimit(self):
+        return self.request("get", "/mining/boost/check/energyLimit", "energyLimit")['energyLimit']
 
-    async def upgrade_reChargeSpeed(self):
-        response = await self.request("get", "/mining/boost/check/reChargeSpeed", "reChargeSpeed")
-        return response['reChargeSpeed']
+    def upgrade_reChargeSpeed(self):
+        return self.request("get", "/mining/boost/check/reChargeSpeed", "reChargeSpeed")['reChargeSpeed']
